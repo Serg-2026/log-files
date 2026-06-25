f036217833d7e6523a07ad28a4c42787eeffe76e
Serg-2026 <tnatalka2018@gmail.com>
2026-06-25 15:37:50 +0300

    Fix POPCAT address for Solana
    Updated the address for POPCAT on the Solana chain.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ pd_cleaner.set_page_config(page_title="Multi-Asset Analytics Board", layout="wid
 # Словарь наших монет с их контрактами и сетями
 TOKENS = {
     "SKYAI (BNB Chain)": {"address": "0x92aa03137385F18539301349dcfC9EbC923fFb10", "chain": "bsc"},
-    "POPCAT (Solana)": {"address": "7gc14MGBwX89LSv9WuWw5fWRLDZFC8C2u167meQHpump", "chain": "solana"},
+    "POPCAT (Solana)": {"address": "7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr", "chain": "solana"},
     "RE (Ethereum)": {"address": "0xbbC44297136Bb6292323aAA74360e2B325Be83D2", "chain": "ethereum"}
 }
 
